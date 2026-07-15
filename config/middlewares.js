f4ad17174fb7951bfd1293160fb0162927ee6c7d
Arjun Poudel <arjunpoudel479@gmail.com>
2026-07-16 00:06:11 +0545

    ..

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,34 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+
+
+'img-src': [
+  "'self'",
+  'data:',
+  'blob:',
+  'https://tiredeals-media-assets.sfo3.digitaloceanspaces.com',
+],
+
+'media-src': [
+  "'self'",
+  'data:',
+  'blob:',
+  'https://tiredeals-media-assets.sfo3.digitaloceanspaces.com',
+],
+
+        },
+      },
+    },
+  },
+
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
